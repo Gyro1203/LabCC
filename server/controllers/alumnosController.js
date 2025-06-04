bca054684f634f236a77a21b5a64b7fc07e6064b
Gyro1203 <sebastian.vasquez2101@alumnos.ubiobio.cl>
2025-06-03 23:41:15 -0400

    refactor(services): Se comienza a mover la logica de los controladores a los servicios

--- server/controllers/alumnosController.js
+++ server/controllers/alumnosController.js
@@ -1,28 +1,26 @@
 "use strict";
 import db from '../config/db.js';
 import { handleSuccess, handleErrorClient, handleErrorServer } from '../handlers/responseHandlers.js';
-import { getAlumnosService } from '../services/alumnos.services.js';
+import { getAlumnoService, getAlumnosService, createAlumnoService, updateAlumnoService, deleteAlumnoService } from '../services/alumnos.services.js';
 
 export const getAlumnos = async (req, res) => {
     try {
         const [alumnos, errorAlumnos] = await getAlumnosService();
-        if(errorAlumnos) return handleErrorClient(res, 404, errorAlumnos.message)
+        if(errorAlumnos) return handleErrorClient(res, 404, errorAlumnos);
         alumnos.length === 0 
             ? handleSuccess(res, 204)
             : handleSuccess(res, 200, "Registro de alumnos encontrado", alumnos);
     } catch (error) {
-        console.error("Error al obtener alumnos", error);
         handleErrorServer(res, 500, error.message);
     }
 };
 
 export const getAlumno = async (req, res) => {
     try {
-        const [result] = await db.query("SELECT * FROM alumnos WHERE id_alumno = ?", [req.params.id]);
-        if (result.length <= 0) return handleErrorClient(res, 404, "No se encontró al alumno");
-        handleSuccess(res, 200, "Alumno encontrado", result[0]);
+        const [alumno, errorAlumno] = await getAlumnoService([req.params.id]);
+        if (errorAlumno) return handleErrorClient(res, 404, errorAlumno);
+        handleSuccess(res, 200, "Alumno encontrado", alumno);
     } catch (error) {
-        console.error("Error al obtener alumno", error);
         handleErrorServer(res, 500, error.message);
     }
 };
@@ -31,13 +29,14 @@ export const getAlumno = async (req, res) => {
 export const createAlumno = async (req, res) => {
     try {
         const { nombre, rut, carrera, facultad, departamento } = req.body;
-        const [result] = await db.query("INSERT INTO alumnos (nombre, rut, carrera, facultad, departamento) VALUES (?, ?, ?, ?, ?)", [nombre, rut, carrera, facultad, departamento]);
-        console.log(result);
+        const [alumno, errorAlumno] = await createAlumnoService({ nombre, rut, carrera, facultad, departamento });
+        if(errorAlumno) return handleErrorClient(res, 400, "Error al crear alumno", errorAlumno);
+        console.log(alumno);
         handleSuccess(
             res,
             201,
             "Alumno creado exitosamente",
-            { id: result.insertId, nombre, rut, carrera, facultad, departamento}
+            alumno
         );
         //res.status(201).json({ id: result.insertId, nombre, rut, carrera, facultad, departamento });
     } catch (error) {
@@ -48,24 +47,20 @@ export const createAlumno = async (req, res) => {
 
 export const updateAlumno = async (req, res) => {
     try {
-        const [result] = await db.query("UPDATE alumnos SET ? WHERE id_alumno = ?", [req.body, req.params.id]);
-        console.log(result);
-        if (result.affectedRows === 0) return    handleErrorClient(res, 404, "No se encontró al alumno");
-        handleSuccess(res, 200, "Datos del alumno actualizados", result);
-        //Corregir, buscar por id, mostrar datos nuevos.
+        const [alumno, errorAlumno] = await updateAlumnoService(req.body, req.params.id);
+        if(errorAlumno) return handleErrorClient(res, 404, errorAlumno);
+        handleSuccess(res, 200, "Datos del alumno actualizados", alumno);
     } catch (error) {
-        console.error("Error al modificar al alumno", error);
         handleErrorServer(res, 500, error.message);
     }
 };
 
 export const deleteAlumno = async (req,res) => {
     try {
-        const [result] = await db.query("DELETE FROM alumnos WHERE id_alumno = ?", [req.params.id]);
-        if (result.affectedRows === 0) return handleErrorClient(res, 404, "Error elimando al alumno", "No se encontró al alumno");
-        handleSuccess(res, 200, "Se elimino el registro del alumno exitosamente");
+        const [alumno, errorAlumno] = await deleteAlumnoService(req.params.id);
+        if (errorAlumno) return handleErrorClient(res, 404, "Error elimando al alumno", errorAlumno);
+        handleSuccess(res, 200, "Se elimino el registro del alumno exitosamente", alumno);
     } catch (error) {
-        console.error("Error eliminando al alumnos", error);
         handleErrorServer(res, 500, error.message);
     }
 };
--- server/services/alumnos.services.js
+++ server/services/alumnos.services.js
@@ -1,14 +1,79 @@
 "use strict";
 import db from '../config/db.js';
 
-export const getAlumnosService = async (req, res) => {
+export const getAlumnosService = async () => {
     try {
         const [result] = await db.query("SELECT * FROM alumnos");
         if(!result || result.length === 0) return [null, "No hay alumnos registrados"];
-        // const alumnosData = result.map(({ id_alumno, ...alumno }) => alumno)
         return [result, null];
-        // res.json(result);
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        console.error("Error al obtener alumnos:", error);
+        return [null, "Error interno del servidor"];
     }
 };
+
+export const getAlumnoService = async ( id ) => {
+    try {
+        const [result] = await db.query("SELECT * FROM alumnos WHERE id_alumno = ?", id);
+        if (!result || result.length === 0) return [null, "No se encontró al alumno"];
+        return [result[0], null];
+    } catch (error) {
+        console.error("Error al obtener alumno", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+export const createAlumnoService = async ( body ) => {
+    try {
+        const { nombre, rut, carrera, facultad, departamento } = body;
+        const [existeAlumno] = await db.query("SELECT * FROM alumnos WHERE rut = ?", rut);
+
+        if(existeAlumno[0]) return [null, "Ya existe un alumno con el rut ingresado"];
+
+        const [result] = await db.query("INSERT INTO alumnos (nombre, rut, carrera, facultad, departamento) VALUES (?, ?, ?, ?, ?)", [nombre, rut, carrera, facultad, departamento]);
+        if(result.affectedRows === 0) return [null, "Error en la creación"];
+
+        const [created] = await db.query("SELECT * FROM alumnos WHERE rut = ?", rut);
+        if(!created) return [null, "Alumno no encontrado despues de ser creado"];
+
+        return [created, null];
+    } catch (error) {
+        console.error("Error al crear un alumno", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+export const updateAlumnoService = async ( body, id ) => {
+    try {
+        const [actualizarAlumno] = await db.query("SELECT * FROM alumnos WHERE id_alumno = ?", id);
+        if(!actualizarAlumno || actualizarAlumno.length === 0) return [null, "No se encontró al alumno"];
+
+        const [existeAlumno] = await db.query("SELECT * FROM alumnos WHERE rut = ?", body.rut);
+
+        if(existeAlumno[0] && existeAlumno[0].rut !== actualizarAlumno[0].rut) 
+            return [null, "Ya existe un alumno con el rut ingresado"];
+
+        await db.query("UPDATE alumnos SET ? WHERE id_alumno = ?", [body, id]);
+
+        const [alumno] = await db.query("SELECT * FROM alumnos WHERE id_alumno = ?", id);
+        if(!alumno) return [null, "Alumno no encontrado despues de actualizar"];
+
+        return [alumno[0], null];
+    } catch (error) {
+        console.error("Error al modificar al alumno", error);
+        return [null, "Error interno del servidor"];
+    }
+}
+
+export const deleteAlumnoService = async ( id ) => {
+    try {
+        const [result] = await db.query("SELECT * FROM alumnos WHERE id_alumno = ?", id);
+        if (!result || result.length === 0) return [null, "No se encontró al alumno"];
+        const [deleted] = await db.query("DELETE FROM alumnos WHERE id_alumno = ?", id);
+        if(deleted.affectedRows === 0) return [null, "No se pudo borrar al alumno"];
+        return [result[0], null];
+    } catch (error) {
+        console.error("Error eliminando al alumnos", error);
+        return [null, "Error interno del servidor"];
+    }
+}
