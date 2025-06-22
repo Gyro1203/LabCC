5b227eb27c4fc7b1c1f1f339b7dffc7e3a18b5c7
Fuumont <andres.azuaje2101@alumnos.ubiobio.cl>
2025-06-21 21:04:32 -0400

    creacion backend de usuarios

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import ensayosRoutes from './routes/ensayos.routes.js';
 import ingresosRoutes from './routes/ingresos.routes.js'
 import actividadesRoutes from './routes/actividades.routes.js'
 import asistenciasRoutes from './routes/asistencias.routes.js'
+import usuariosRoutes from './routes/usuarios.routes.js'
 import { HOST, PORT } from './config/configEnv.js';
 
 const app = express();
@@ -17,7 +18,7 @@ app.use(ensayosRoutes);
 app.use(ingresosRoutes);
 app.use(actividadesRoutes);
 app.use(asistenciasRoutes);
-
+app.use(usuariosRoutes);
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en ${HOST}:${PORT}`);
 });
